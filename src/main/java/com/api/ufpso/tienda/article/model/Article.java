976a94efc501c31b59a0503b82964d7192b0b55f
Jhoiner <jjmartinezs@ufpso.edu.co>
2023-11-10 21:43:27 -0500

    implementacion restricciones articulo y categoria

--- src/main/java/com/api/ufpso/tienda/article/model/Article.java
+++ src/main/java/com/api/ufpso/tienda/article/model/Article.java
@@ -2,6 +2,9 @@ package com.api.ufpso.tienda.article.model;
 
 import com.api.ufpso.tienda.category.model.Category;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.sql.Date;
@@ -13,13 +16,22 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "name is required")
+    @Size(max = 255, message = "name max 255 characters")
     private String name;
+    @NotNull(message = "description is required")
+    @Size(max = 255, message = "description max 255 characters")
     private String description;
+    @NotNull(message = "stock is required")
+    @Size(min = 5, max = 15, message = "stock max 15 characters and min 1")
     private Number stock;
+    @NotNull(message = "price is required")
+    @Size(min = 5, max = 15, message = "price max 15 characters and min 1")
     private String price;
+    @NotNull(message = "dateOfAdmission is required")
     private Date dateOfAdmission;
-
     @ManyToOne
     @JoinColumn(name = "category")
     private Category category;
+
 }
--- src/main/java/com/api/ufpso/tienda/category/model/Category.java
+++ src/main/java/com/api/ufpso/tienda/category/model/Category.java
@@ -1,6 +1,8 @@
 package com.api.ufpso.tienda.category.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -11,6 +13,8 @@ public class Category {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "nameCategory name is required")
+    @Size(max = 255, message = "nameCategory name max 255 characters")
     private String nameCategory;
 
 }
